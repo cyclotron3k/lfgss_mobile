980fa5a58a3df144b145741af1a641736c1c46e8
cyclotron3k <aidan.samuel@gmail.com>
2023-10-03 02:11:17 +1100

    v1.0.7

--- lib/services/microcosm_client.dart
+++ lib/services/microcosm_client.dart
@@ -30,7 +30,7 @@ class _ExpiringResponse {
 }
 
 class MicrocosmClient {
-  static const String userAgent = "LFGSSMobile/1.0.6 (android;cyclotron3k)";
+  static const String userAgent = "LFGSSMobile/1.0.7 (android;cyclotron3k)";
   static final MicrocosmClient _singleton = MicrocosmClient._internal();
   String? accessToken;
 
